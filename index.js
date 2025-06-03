72d870a81e396cd8792e8f2039468048b522e2c8
birhanu <birhanutbelay@gmail.com>
2025-06-03 11:23:45 +0300

    Updated Bank system code using python file

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ document.querySelectorAll("h3").forEach(h3 => {
 });
 const fruitList = document.getElementById("fruList");
 const newFruit = document.createElement("li");
-newFruit.textContent = "Oranges";
+newFruit.textContent = "Papaya";
 fruitList.appendChild(newFruit);
 
 const vegList = document.getElementById("vegList");
-const newVeg = document.createElement("li");
-newVeg.textContent = "Spinach";
-vegList.appendChild(newVeg)
+const newVegetable = document.createElement("li");
+newVegetable.textContent = "Spinach";
+vegList.appendChild(newVegetable)
 
 function addImageToListItem(listItem, imagePath) {
     const img = document.createElement("img");
@@ -27,15 +27,15 @@ function addImageToListItem(listItem, imagePath) {
 }
 
 const fruitItems = document.querySelectorAll("#fruList li");
-const vegItems = document.querySelectorAll("#vegList li");
+const vegetableItems = document.querySelectorAll("#vegList li");
 
 addImageToListItem(fruitItems[0], "image/mango.jpg");
 addImageToListItem(fruitItems[1], "image/banana.jpg");
 addImageToListItem(fruitItems[2], "image/water_melon.jpg");
-addImageToListItem(fruitItems[3], "image/orange.jpg");
+addImageToListItem(fruitItems[3], "image/papaya.jpg");
 
 
-addImageToListItem(vegItems[0], "image/onion.jpg");
-addImageToListItem(vegItems[1], "image/tomato.jpg");
-addImageToListItem(vegItems[2], "image/kales.jpeg");
-addImageToListItem(vegItems[3], "image/spinach.avif");
+addImageToListItem(vegetableItems[0], "image/onion.jpg");
+addImageToListItem(vegetableItems[1], "image/tomato.jpg");
+addImageToListItem(vegetableItems[2], "image/kales.jpeg");
+addImageToListItem(vegetableItems[3], "image/cocumber.jpeg");
